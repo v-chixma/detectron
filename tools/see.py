586323e87d18ec1ee862c767073ec302d1e807f0
yxzhu <zyxsa@mail.ustc.edu.cn>
2018-03-29 19:23:50 +0800

    test rpn-frcn-mask iter60000

--- tools/see.py
+++ tools/see.py
@@ -3,8 +3,8 @@ import numpy as np
 import pdb
 import glob
 import os
-path='./tmp/debug_infer_minrect_iter9_merge/'
-txts=glob.glob(path+'Task1*')
+path='./tmp/rpn-frcn-mask_iter6_merge/'
+txts=glob.glob(path+'Task1_baseball*')
 for i_txt in range(len(txts)):
     #if i_txt<3:
     #    continue
@@ -32,9 +32,13 @@ for i_txt in range(len(txts)):
         for poly in polys:
 
            cv2.polylines(im_draw,[poly.astype(np.int32).reshape([-1,1,2])],True,(0,0,255),3)
-        if not os.path.exists('./tmp/see_result/'+os.path.basename(txt)[:-4]):
-            os.makedirs('./tmp/see_result/'+os.path.basename(txt)[:-4])
-        cv2.imwrite('./tmp/see_result/'+os.path.basename(txt)[:-4]+'/'+im+'.jpg',im_draw)
+        if not os.path.exists('./tmp/see_result2/'+os.path.basename(txt)[:-4]):
+            os.makedirs('./tmp/see_result2/'+os.path.basename(txt)[:-4])
+        print './tmp/see_result2/'+os.path.basename(txt)[:-4]+'/'+im+'.jpg\n'
+        #cv2.imshow(im_draw)
+        #cv2.waitKey(0)
+        cv2.imwrite('./tmp/see_result2/'+os.path.basename(txt)[:-4]+'/'+im+'.jpg\n',im_draw)
+        #'/home/yxzh/mount_31/cxma/software/detectron/tmp/see_result2/'+os.path.basename(txt)[:-4]+'/'+im+'.jpg'
 
     #pdb.set_trace()
 
